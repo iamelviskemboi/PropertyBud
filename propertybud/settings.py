371200e5b476d3f1bf48755cfdeccd940d550585
Elvis Kemboi <iamelviskemboi@gmail.com>
2020-05-26 10:17:35 +0300

    DEBUG = False

--- propertybud/settings.py
+++ propertybud/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fto76pz67_#j8jyrqcepk^li#_udsmeov3g6)-njtl3fn4rlyv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'propertybud.herokuapp.com']
 
